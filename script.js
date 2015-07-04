0f64e381000f79b4c1529b06d53d3a45becc75c2
David Bengsch <db72432@gmail.com>
2015-07-04 13:52:16 +0200

    Event form

--- script.js
+++ script.js
@@ -3,38 +3,37 @@ var kuzzle = new Kuzzle("http://api.uat.kuzzle.io:7512");
 angular.module("chooseyourday", [])
   .controller("chooseyourdayCtrl", ["$scope", function($scope) {
 
-    $scope.newChooseYourDay = null;
-    $scope.chooseyourdays = [];
+    $scope.newEvent = null;
+    $scope.events = [];
 
     $scope.init = function() {
-      getAllChooseYourDay();
+      getAllEvents();
 
-      kuzzle.subscribe("chooseyourday", {term: {type:"chooseyourday"}}, function(response) {
+      kuzzle.subscribe("chooseyourday_event", {term: {type:"chooseyourday_event"}}, function(response) {
         if(response.action === "create") {
-          var newChooseYourDay = {
+          var newEvent = {
             _id: response._id,
-            title: response.body.title,
+            name: response.body.name,
             dates: response.body.dates,
             done: response.body.done
           };
 
-          addToList(newChooseYourDay);
+          addToList(newEvent);
         }
 
         if(response.action === "delete") {
-          $scope.chooseyourdays.some(function(chooseyourday, index) {
-            if(chooseyourday._id === response._id) {
-              $scope.chooseyourdays.splice(index, 1);
+          $scope.events.some(function(event, index) {
+            if(event._id === response._id) {
+              $scope.events.splice(index, 1);
               return true;
             }
           });
         }
 
         if(response.action === "update") {
-          $scope.chooseyourdays.some(function(chooseyourday, index) {
-            if(chooseyourday._id === response._id) {
-              console.log($scope.chooseyourdays[index]);
-              $scope.chooseyourdays[index].done = response.body.done;
+          $scope.events.some(function(event, index) {
+            if(event._id === response._id) {
+              $scope.events[index].done = response.body.done;
               return true;
             }
           });
@@ -43,49 +42,63 @@ angular.module("chooseyourday", [])
         $scope.$apply();
       });
     };
+    
+    $scope.createEvent = function() {
+      if (typeof $scope.newEvent == "undefined" || $scope.newEvent === null) {
+        $scope.newEvent = {};
+      }
 
-    $scope.addChooseYourDay = function() {
-      kuzzle.create("chooseyourday", {type: "chooseyourday", date: $scope.newChooseYourDay.date, title: $scope.newChooseYourDay.title, dates: $scope.newChooseYourDay.dates, done: false}, true);
-      $scope.newChooseYourDay = null;
+      if (typeof $scope.newEvent.dates == "undefined") {
+        $scope.newEvent.dates = [];
+      }
+      $scope.newEvent.dates[0] = { value: "" };
+    };
+    
+    $scope.cancelNewEvent = function() {
+      $scope.newEvent = null;
+    }
+
+    $scope.addEvent = function() {
+      kuzzle.create("chooseyourday_event", {type: "chooseyourday_event", date: $scope.newEvent.date, name: $scope.newEvent.name, dates: $scope.newEvent.dates, done: false}, true);
+      $scope.newEvent = null;
+    };
+    
+    $scope.hasNewEvent = function() {
+      return (typeof $scope.newEvent != "undefined" && $scope.newEvent !== null);
+    };
+
+    $scope.noNewEvent = function() {
+      return !$scope.hasNewEvent();
     };
 
     $scope.addADay = function() {
-      if (typeof $scope.newChooseYourDay == "undefined" || $scope.newChooseYourDay === null) {
-        $scope.newChooseYourDay = {};
-      }
-
-      if (typeof $scope.newChooseYourDay.dates == "undefined") {
-        $scope.newChooseYourDay.dates = [];
-      }
-
-      var i = $scope.newChooseYourDay.dates.length;
-      $scope.newChooseYourDay.dates[i] = { value: "" };
-    }
+      var i = $scope.newEvent.dates.length;
+      $scope.newEvent.dates[i] = { value: "" };
+    };
 
     $scope.toggleDone = function(index) {
-      kuzzle.update("chooseyourday", {_id: $scope.chooseyourdays[index]._id, done: !$scope.chooseyourdays[index].done});
+      kuzzle.update("chooseyourday_event", {_id: $scope.events[index]._id, done: !$scope.events[index].done});
     };
 
     $scope.delete = function(index) {
-      kuzzle.delete("chooseyourday", $scope.chooseyourdays[index]._id);
+      kuzzle.delete("chooseyourday_event", $scope.events[index]._id);
     };
 
-    var addToList = function(chooseyourday) {
-      $scope.chooseyourdays.push(chooseyourday);
+    var addToList = function(event) {
+      $scope.events.push(event);
     };
 
-    var getAllChooseYourDay = function() {
-      kuzzle.search("chooseyourday", {}, function(response) {
-        response.result.hits.hits.forEach(function(chooseyourday) {
-          var newChooseYourDay = {
-            _id: chooseyourday._id,
-            title: chooseyourday._source.title,
-            dates: chooseyourday._source.dates,
-            done: chooseyourday._source.done
+    var getAllEvents = function() {
+      kuzzle.search("chooseyourday_event", {}, function(response) {
+        response.result.hits.hits.forEach(function(event) {
+          var newEvent = {
+            _id: event._id,
+            title: event._source.name,
+            dates: event._source.dates,
+            done: event._source.done
           };
 
-        console.log(chooseyourday);
-          $scope.chooseyourdays.push(newChooseYourDay);
+          $scope.events.push(newEvent);
         });
 
         $scope.$apply();
