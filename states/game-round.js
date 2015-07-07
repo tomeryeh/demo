7718bcdfc704d422b5c5b6adca19dad1c34826f2
Samuel Bouic <sbouic@kaliop.com>
2015-07-07 19:23:42 +0200

    Added LASERRRRRRSgits :D

--- states/game-round.js
+++ states/game-round.js
@@ -1,8 +1,13 @@
+var direction = 'right';
 var jumpTimer = 0;
+var shootTimer = 0;
+var lasers = [];
 var readyForDoubleJump = false;
 var doubleJumped = false;
 var emitterLifeSpan = 0;
 var flying = false;
+var decorHP = 100;
+var groundPounding = false;
 function GameRoundState() {}
 GameRoundState.prototype = {
     init: function(initData) {
@@ -13,6 +18,7 @@ GameRoundState.prototype = {
         //game.load.tilemap('level1', 'assets/games/starstruck/level1.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('player', 'assets/sprites/game-round/player.png');
         game.load.image('smoke-particle', 'assets/sprites/game-round/smoke-particle.png');
+        game.load.image('laser', 'assets/sprites/game-round/laser.png');
         /*game.load.spritesheet('dude', 'assets/games/starstruck/dude.png', 32, 48);
         game.load.spritesheet('droid', 'assets/games/starstruck/droid.png', 32, 32);
         game.load.image('starSmall', 'assets/games/starstruck/star.png');
@@ -35,17 +41,22 @@ GameRoundState.prototype = {
 
         player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
         player.anchor.setTo(0.5, 0.5);
-        player.height = 50;
-        player.width = 30;
+        player.height = 85;
+        player.width = 64;
 
-        decor = game.add.sprite(game.world.centerX / 2, (game.world.centerY * 2) - 50, 'player');
+        decor = game.add.sprite(game.world.centerX / 2, (game.world.centerY * 2) - 85, 'player');
         decor.anchor.setTo(0.5, 0.5);
-        decor.height = 100;
-        decor.width = 60;
+        decor.height = 170;
+        decor.width = 128;
+        game.physics.enable(decor, Phaser.Physics.ARCADE);
+        decor.body.bounce.setTo(0.2, 0.2);
+        decor.body.collideWorldBounds = true;
+
+        var style = {font: '64px Arial', fontWeight: 'bold', fill: "#FFF", align: "center"};
+        decorHPText = game.add.text(decor.x - 64, decor.y - 170, "100", style);
 
         game.physics.enable(player, Phaser.Physics.ARCADE);
-        player.body.bounce.x = 0.8;
-        player.body.bounce.y = 0.5;
+        player.body.bounce.setTo(0.8, 0.5);
         player.body.collideWorldBounds = true;
 
         //game.camera.follow(player);
@@ -55,13 +66,12 @@ GameRoundState.prototype = {
         emitter.setXSpeed(0, 0);
         emitter.setYSpeed(0, 0);
         emitter.setAlpha(1, 0, 3000);
-        emitter.setScale(0.0, 1, 0.0, 1, 6000, Phaser.Easing.Quintic.Out);
+        emitter.setScale(0.0, 1.2, 0.0, 1.2, 6000, Phaser.Easing.Quintic.Out);
         emitter.gravity = -800;
-        emitter.emitX = 64;
-        emitter.emitY = 500;
 
         cursors = game.input.keyboard.createCursorKeys();
-        jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
         /* END GAME LOGIC */
 
@@ -75,16 +85,21 @@ GameRoundState.prototype = {
     updatePlayers: function() {
         emitter.emitX = player.x;
         emitter.emitY = player.y;
+
         if(player.body.velocity.x > 0)
             player.body.velocity.x = player.body.velocity.x - 5;
         if(player.body.velocity.x < 0)
             player.body.velocity.x = player.body.velocity.x + 5;
+
         if (cursors.left.isDown) {
+            direction = 'left';
             player.body.velocity.x = -300;
         }
         if(cursors.right.isDown) {
+            direction = 'right';
             player.body.velocity.x = 300;
         }
+
         if(player.body.onFloor()) {
             player.body.gravity.y = 1000;
         }
@@ -106,23 +121,85 @@ GameRoundState.prototype = {
             emitter.start(false, 4000, 20);
             emitterLifeSpan = 30;
         }
+
         if(emitterLifeSpan > 0) {
             emitterLifeSpan = emitterLifeSpan - 1;
         } else {
             player.body.gravity.y = 300;
             emitter.on = false;
         }
+
         if(flying && player.body.onFloor()) {
             flying = false;
             game.juicy.shake(30, 100);
+            //game.juicy.jelly(player, 1.5, 100);
             this.kuzzleFlash(1, 2000);
-            this.tweenTint(player, player.color, 0xFF0000, 500);
-            this.tweenTint(decor, decor.color, 0xFF0000, 500);
+            this.tweenTint(player, 0x333333, 0xFF11FF, 500);
+            this.tweenTint(decor, 0x333333, 0xFF11FF, 500);
         }
 
-        if(cursors.up.isDown) {
-            player.body.velocity.x = 200;
+        if(cursors.down.isDown) {
+            player.body.gravity.y = 10000;
+            groundPounding = true;
+        } else {
+            player.body.gravity.y = 100;
+            groundPounding = false;
         }
+
+        if(fireButton.isDown && game.time.now > shootTimer) {
+            shootTimer = game.time.now + 50;
+            this.shootLaser();
+        }
+
+        lasers.forEach(function(e) {
+            game.physics.arcade.collide(e, decor, self.decorTakesDamage, null, self);
+        });
+        game.physics.arcade.collide(player, decor, self.decorTakesDamageFromGroundPound, null, self);
+
+        decorHPText.x = decor.x - 64;
+        decorHPText.y = decor.y - 170;
+        decorHPText.text = decorHP;
+        decorHPText.tint = Phaser.Color.interpolateColor(0xFF0000, 0xFFFFFF, 100, decorHP);
+    },
+    decorTakesDamageFromGroundPound: function() {
+        if(groundPounding && flying) {
+            flying = false;
+            decorHP -= 10;
+            this.tweenTint(decor, 0x333333, 0xFF11FF, 100);
+            if(decorHP <= 0) {
+                this.decorDies();
+            }
+        }
+    },
+    decorTakesDamage: function(l, d) {
+        l.destroy();
+        decorHP -= 1;
+        this.tweenTint(decor, 0x333333, 0xFF11FF, 100);
+        if(decorHP <= 0) {
+            this.decorDies();
+        }
+    },
+    decorDies: function() {
+        decorHPText.destroy();
+        var dieAnimation = game.add.tween(decor).to({alpha: 0.0}, 500, 'Linear').start();
+        dieAnimation.onComplete.add(function() { decor.destroy() });
+    },
+    shootLaser: function() {
+        var laser = game.add.sprite(player.x, player.y, 'laser');
+        laser.width = 100;
+        laser.height = 40;
+        game.physics.enable(laser, Phaser.Physics.ARCADE);
+        laser.checkWorldBounds = true;
+        laser.body.gravity.x = -1000;
+        laser.body.gravity.y = -1000;
+        laser.x = player.x;
+        laser.y = player.y;
+
+        if(direction == 'right')
+            laser.body.velocity.x = 6000;
+        if(direction == 'left')
+            laser.body.velocity.x = -6000;
+        lasers.push(laser);
     },
     kuzzleFlash: function(maxAlpha, duration) {
         maxAlpha = maxAlpha || 1;
@@ -146,10 +223,13 @@ GameRoundState.prototype = {
         });
         colorTween.onComplete.add(function() {
             obj.tint = 0xFFFFFF;
+            self.game.stage.setBackgroundColor(0x000000);
         });
 
         // set the object to the start color straight away
         obj.tint = startColor;
+
+        self.game.stage.setBackgroundColor(0xFF0000);
 
         // start the tween
         colorTween.start();
