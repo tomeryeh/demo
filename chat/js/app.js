5d52438647c2c7c2f83248f67ee678f7d9c11649
scottinet <scottinet@kaliop.com>
2015-08-11 17:26:49 +0200

    chat demo: major overhaul

--- chat/js/app.js
+++ chat/js/app.js
@@ -1,39 +1,112 @@
 var
   kuzzle,
-  messageFlow = $('#messageFlow'),
-  userList = $('#userList'),
-  inputArea = $('#inputChatMessage'),
-  GLOBAL_CHAT_ROOM = 'demo-kuzzle-chat';
+  whoami = {},
+  subcriptionId,
+  CHAT_MSG_COLLECTION = 'demo-chat-messages',
+  CHAT_ROOM_COLLECTION = 'demo-chat-chatrooms',
+  GLOBAL_CHAT_ROOMID = undefined,
+  GLOBAL_CHAT_ROOM = 'main room';
 
 function startDemo(kuzzleUrl) {
   kuzzle = Kuzzle.init(kuzzleUrl);
 
-  createUsername(GLOBAL_CHAT_ROOM, function(error, whoami) {
-    listenMessages(GLOBAL_CHAT_ROOM);
-    listenUserActivity(GLOBAL_CHAT_ROOM, whoami);
+  whoami = {
+    userName: 'Anonymous',
+    userColor: '#' + Math.floor(Math.random() * 16777215).toString(16),
+    chatRoom: GLOBAL_CHAT_ROOM
+  };
 
-    displayConnectedUsers();
+  updateAliasButton();
+  listenMessages(GLOBAL_CHAT_ROOM);
+  refreshRooms();
+  setInterval(function() { refreshRooms(); }, 2000);
+  setInterval(function() { updateUserCount(); }, 500);
 
-    // Remove our user entry when we leave
-    window.onbeforeunload = function () {
-      kuzzle.delete('chatRegister', whoami.id);
-    };
+  // Clicking the alias button asks the user for a new alias
+  $('#changeAlias').click(function (e) {
+    var newName;
 
-    // Clicking on the 'Send message' button sends the message
-    $('#sendChatMessage').click(function (e) {
-      e.preventDefault();
-      sendMessage(whoami, inputArea.val());
-      inputArea.val("");
-    });
+    e.preventDefault();
+    newName = prompt('Please enter your name', whoami.userName);
 
-    // Pressing enter in the input area sends the message
-    inputArea.on('keydown', function (e) {
-      // (ENTER key)
-      if (e.which == 13) {
-        sendMessage(whoami, inputArea.val());
-        inputArea.val("");
+    if (newName) {
+      whoami.userName = newName;
+      updateAliasButton();
+    }
+  });
+
+  // Clicking on the 'Send message' button sends the message
+  $('#sendChatMessage').click(function (e) {
+    e.preventDefault();
+    sendMessage();
+  });
+
+  // Pressing enter in the input area sends the message
+  $('#inputChatMessage').on('keydown', function (e) {
+    // (ENTER key)
+    if (e.which == 13) {
+      sendMessage();
+    }
+  });
+
+  // Add a new room
+  $('#createRoom').click(function (e) {
+    var newRoom;
+
+    e.preventDefault();
+    newRoom = prompt('New room name');
+
+    if (newRoom) {
+      kuzzle.count(CHAT_ROOM_COLLECTION, {query: {match: {name: newRoom}}}, function (error, response) {
+        if (response.count > 0) {
+          alert('The room ' + newRoom + ' already exists');
+        }
+        else {
+          kuzzle.create(CHAT_ROOM_COLLECTION, {name: newRoom}, true, function (error, room) {
+            switchRoom(room._id, newRoom);
+          });
+        }
+      });
+    }
+  });
+
+  $('#deleteRoom').click(function (e) {
+    e.preventDefault();
+
+    if (whoami.chatRoom === GLOBAL_CHAT_ROOM) {
+      alert('You may only remove rooms other than the main one');
+      return;
+    }
+
+    kuzzle.countSubscription(subcriptionId, function (error, response) {
+      var room = whoami.chatRoom;
+      if (response > 1) {
+        alert('You can\'t remove a non-empty room. Please ask people to leave first!');
+      } else {
+        kuzzle.delete(CHAT_ROOM_COLLECTION, whoami.chatRoomId, function (error, response) {
+          alert('Room ' + whoami.chatRoom + ' removed');
+          switchRoom(GLOBAL_CHAT_ROOMID, GLOBAL_CHAT_ROOM);
+        });
       }
     });
+  });
+}
+
+function updateAliasButton() {
+  $('#alias').text(whoami.userName);
+}
+
+/**
+* Get the number of users listening to the current chat room, and update
+* the introductory message with it
+*/
+function updateUserCount() {
+  kuzzle.countSubscription(subcriptionId, function (error, response) {
+    if (error) {
+      throw new Error(error);
+    }
+
+    $('#userCount').text('There are ' + response + ' users connected to [' + whoami.chatRoom + ']');
   });
 }
 
@@ -44,7 +117,13 @@ function startDemo(kuzzleUrl) {
 * @param {string} chatRoom is the name of the chat room
 */
 function listenMessages(chatRoom) {
-  kuzzle.subscribe('chatMessage', {term: {chatRoom: chatRoom}}, function (error, newMessage) {
+  if (subcriptionId) {
+    kuzzle.unsubscribe(subcriptionId);
+  }
+
+  subcriptionId = kuzzle.subscribe(CHAT_MSG_COLLECTION, {term: {chatRoom: chatRoom}}, function (error, newMessage) {
+    var messageFlow = $('#messageFlow');
+
     if (error) {
       throw new Error(error);
     }
@@ -68,93 +147,32 @@ function listenMessages(chatRoom) {
 }
 
 /**
-* Listen to users entering/leaving the chat room.
+* Retrieve and display existing rooms
 */
-function listenUserActivity(chatRoom, user) {
-  kuzzle.subscribe('chatRegister', {not: {term: {username: user.username}}}, function (error, userActivity) {
+function refreshRooms() {
+  $('#roomList').empty();
+
+  kuzzle.search(CHAT_ROOM_COLLECTION, {}, function (error, rooms) {
     if (error) {
       throw new Error(error);
     }
-console.log('DATA RECEIVED: ');
-console.dir(userActivity);
-    switch (userActivity.action) {
-      case 'create':
-        userList.append(
-          '<div class="user" style="color: ' +
-          userActivity._source.chatColor +
-          ';" data-userID="' +
-          userActivity._id +
-          '">' +
-          userActivity._source.username +
-          '</div>'
-        );
-        break;
-      case 'delete':
-        userList.find('[data-userID="' + userActivity._id + '"]').remove();
-        break;
-    }
-  });
-}
-
-/**
-* Ask the user for an alias and create a new entry in Kuzzle
-*
-* The callback is called with a User object containing the following members:
-*   id
-*   username
-*   chatRoom
-*   chatColor
-*/
-function createUsername(chatRoom, callback) {
-  var
-    randomName = Math.floor(Math.random() * 16777215).toString(16),
-    searchPattern,
-    name,
-    userEntry = {
-      chatRoom: chatRoom,
-      chatColor: '#' + randomName
-    };
-
-  do {
-    name = prompt('Please enter your name', randomName);
-  } while(!name);
-
-  searchPattern = {
-    query: {
-      bool: {
-        must: [
-          { match: { username: name } },
-          { match: { chatRoom: chatRoom } }
-        ]
-      }
-    }
-  };
-
-  kuzzle.count('chatRegister', searchPattern, function (error, searchResult) {
-    if (error) {
-      callback(error, null);
-    }
     else {
-      searching = false;
-      if (searchResult.count > 0) {
-        alert('The name ' + name + ' already exist, please choose another one');
-        createUsername(chatRoom, function (error, user) {
-          callback(error, user);
-        });
-        return false;
+      // create a default room if none exist
+      if (rooms.hits.total === 0) {
+        $('#switchRoom').text(GLOBAL_CHAT_ROOM);
+        kuzzle.create(CHAT_ROOM_COLLECTION, {name: GLOBAL_CHAT_ROOM}, true);
       }
       else {
-        userEntry.username = name;
-
-        // Create the new user entry
-        console.log('CREATING USER: ', userEntry);
-        kuzzle.create('chatRegister', userEntry, true, function (error, response) {
-          if (error) {
-            callback(error, response);
+        rooms.hits.hits.forEach(function (room) {
+          if (room._source.name === whoami.chatRoom) {
+            $('#switchRoom').text(room._source.name);
           }
           else {
-            userEntry.id = response._id;
-            callback(null, userEntry);
+            if (room._source.name === GLOBAL_CHAT_ROOM) {
+              GLOBAL_CHAT_ROOMID = room._id;
+            }
+
+            addRoomSwitch(room._id, room._source.name);
           }
         });
       }
@@ -162,42 +180,48 @@ function createUsername(chatRoom, callback) {
   });
 }
 
-/**
-* Retrieve all connected users and display them
-*/
-function displayConnectedUsers() {
-  kuzzle.search('chatRegister', {}, function (error, allUsers) {
-    if (error) {
-      throw new Error(error);
-    }
-    else {
-      allUsers.hits.hits.forEach(function (user) {
-        userList.append(
-          '<div class="user" style="color: ' +
-          user._source.chatColor +
-          ';" data-userID="' +
-          user._source._id +
-          '">' +
-          user._source.username +
-          '</div>'
-        );
-      });
-    }
+// Add a room to the room list, with its switch trigger
+function addRoomSwitch(roomId, roomName) {
+  $('#roomList').append('<li><a id="' + roomId + '" href="#">' + roomName + '</a></li>');
+
+  // Clicking the room button triggers a room Switch
+  $('#' + roomId).click(function(e) {
+    e.preventDefault();
+    switchRoom(roomId, roomName);
   });
 }
 
 /**
 * Send the given content to Kuzzle as a pub/sub message
 */
-function sendMessage(userInfo, content) {
+function sendMessage() {
   var
     message = {
-        content: content,
-        owner: userInfo.username,
-        ownerColor: userInfo.chatColor,
-        chatRoom: userInfo.chatRoom
+        content: $('#inputChatMessage').val(),
+        owner: whoami.userName,
+        ownerColor: whoami.userColor,
+        chatRoom: whoami.chatRoom
     };
 
+    $('#inputChatMessage').val("");
+
   // Broadcast and reset the input
-  kuzzle.create('chatMessage', message);
+  kuzzle.create(CHAT_MSG_COLLECTION, message);
+}
+
+/**
+* Switch to a new room
+*/
+function switchRoom(newRoomId, newRoomName) {
+  if (newRoomName === whoami.chatRoom) {
+    return false;
+  }
+
+  $('#switchRoom').text(newRoomName);
+  whoami.chatRoom = newRoomName;
+  whoami.chatRoomId = newRoomId;
+  listenMessages(newRoomName);
+  refreshRooms();
+
+  $('#messageFlow').append('<div class="message"><small>Entering room [' + newRoomName + ']</small></div>');
 }
