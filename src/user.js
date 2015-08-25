0be8a8ca978de2090129438f149481aecead55da
Eric ALVERNHE <ealvernhe@kd-9Y4KS12.kaliop.loc>
2015-08-25 19:52:55 +0200

    split into module

---
+++ src/user.js
@@ -0,0 +1,53 @@
+////////////////////////user module/////////////////
+
+window.userController = (function() {
+
+	var user = {
+		userId: null,
+		whoami: {
+			type: '',
+			available: true
+		},
+	};
+
+	return {
+		init: function() {
+			return new Promise(
+				function(resolve, reject) {
+					app.userController.fetchFromLocalStorage().then(function(value) {
+						if (value)
+							user = value;
+
+						app.kuzzleController.setUserType(user.whoami.type);
+						app.gisController.setUserType(user.whoami.type);
+						resolve();
+					});
+				});
+		},
+
+		getUser: function() {
+			return user;
+		},
+		fetchFromLocalStorage: function() {
+			return new Promise(
+				function(resolve, reject) {
+					var resolver = Promise.pending();
+					localforage.getItem('cable_user')
+						.then(function(value) {
+							resolve(JSON.parse(value));
+						});
+				});
+
+		},
+		setInLocalStorage: function() {
+			var resolver = Promise.pending();
+			localforage.setItem('cable_user', JSON.stringify(user))
+				.then(function() {
+					resolver.resolve();
+				});
+			return resolver.promise;
+		}
+
+	};
+
+})();
